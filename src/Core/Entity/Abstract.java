6adfa403138d15bc9ef3657ebdd4922a039b6a59
Saif M <sikhlana@gmail.com>
2017-06-01 15:26:01 +0600

    Added all dao for the entities.

--- src/Core/Entity/Abstract.java
+++ src/Core/Entity/Abstract.java
@@ -3,6 +3,8 @@ package Core.Entity;
 import Core.EntityManager;
 import com.j256.ormlite.misc.BaseDaoEnabled;
 
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 
 public abstract class Abstract<T, ID> extends BaseDaoEnabled<T, ID>
@@ -10,5 +12,29 @@ public abstract class Abstract<T, ID> extends BaseDaoEnabled<T, ID>
     public HashMap<String, Object> map()
     {
         return EntityManager.mapEntity(this);
+    }
+
+    @Override
+    public int create() throws SQLException
+    {
+        try
+        {
+            this.getClass().getField("created_at").set(this, new Date());
+        }
+        catch (NoSuchFieldException | IllegalAccessException ignored) { }
+
+        return super.create();
+    }
+
+    @Override
+    public int update() throws SQLException
+    {
+        try
+        {
+            this.getClass().getField("updated_at").set(this, new Date());
+        }
+        catch (NoSuchFieldException | IllegalAccessException ignored) { }
+
+        return super.update();
     }
 }
