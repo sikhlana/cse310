be0412f38353e35ed55cdfb3175e8378beece903
Saif M <sikhlana@gmail.com>
2017-06-01 15:34:24 +0600

    Standardized database fields.

--- db/migration/V4__create_suppliers_table.sql
+++ db/migration/V4__create_suppliers_table.sql
@@ -1,13 +1,16 @@
 CREATE TABLE `suppliers` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `supplier_steet_1` VARCHAR(100) NOT NULL ,
-    `supplier_street_2` VARCHAR(100) NOT NULL ,
-    `supplier_city` VARCHAR(50) NOT NULL ,
-    `supplier_state` VARCHAR(50) NOT NULL ,
-    `supplier_zip` VARCHAR(10) NOT NULL ,
-    `supplier_country` CHAR(2) NOT NULL ,
-    `supplier_name` VARCHAR(100) NOT NULL ,
-    `phone_number` VARCHAR(20) NOT NULL ,
-    `email` VARCHAR(100) NOT NULL ,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+  `id`               INT(10)      NOT NULL AUTO_INCREMENT,
+  `name`             VARCHAR(100) NOT NULL UNIQUE,
+  `address_steet_1`  VARCHAR(100) NOT NULL,
+  `address_street_2` VARCHAR(100) NOT NULL,
+  `address_city`     VARCHAR(50)  NOT NULL,
+  `address_state`    VARCHAR(50)  NOT NULL,
+  `address_zip`      VARCHAR(10)  NOT NULL,
+  `address_country`  CHAR(2)      NOT NULL,
+  `phone_number`     VARCHAR(20)  NOT NULL,
+  `email`            VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
