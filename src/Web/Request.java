f8ad6328f126a06ae792877320107a9ea9005df2
Saif M <sikhlana@gmail.com>
2017-06-24 17:20:41 +0600

    Added parsing of 'dot' nested request parameters.

--- src/Web/Request.java
+++ src/Web/Request.java
@@ -3,6 +3,7 @@ package Web;
 import fi.iki.elonen.NanoHTTPD;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ public class Request
 {
     private NanoHTTPD.IHTTPSession session;
     private Map<String, String> files = new HashMap<>();
+    private Parameters params;
 
     Request(NanoHTTPD.IHTTPSession session) throws InvalidRequestException
     {
@@ -39,6 +41,8 @@ public class Request
                 throw new InvalidRequestException(405);
             }
         }
+
+        params = new Parameters(session.getParameters());
     }
 
     public String getCookie(String key)
@@ -56,9 +60,14 @@ public class Request
         return session.getRemoteIpAddress();
     }
 
-    public List<String> getParam(String key)
+    public Parameters params()
     {
-        return session.getParameters().get(key);
+        return params;
+    }
+
+    public String getParam(String key)
+    {
+        return params().single(key);
     }
 
     public String getFile(String key)
@@ -89,5 +98,94 @@ public class Request
     public boolean isDelete()
     {
         return session.getMethod().equals(NanoHTTPD.Method.DELETE);
+    }
+
+    public class Parameters
+    {
+        private Map<String, Object> params = new HashMap<>();
+
+        Parameters(Map<String, List<String>> p)
+        {
+            for (Map.Entry<String, List<String>> entry : p.entrySet())
+            {
+                for (String value : entry.getValue())
+                {
+                    parse(params, entry.getKey(), value);
+                }
+            }
+        }
+
+        private void parse(Map<String, Object> params, String name, String value)
+        {
+            name = name.replaceAll("\\.{2,}", ".").replaceAll("(^\\.|\\.$)", "");
+
+            if (name.contains("."))
+            {
+                String pieces[] = name.split("\\.", 2);
+
+                if (pieces.length >= 2)
+                {
+                    Map<String, Object> map = null;
+
+                    if (params.containsKey(pieces[0]))
+                    {
+                        if (params.get(pieces[0]) instanceof Map)
+                        {
+                            map = (Map) params.get(pieces[0]);
+                        }
+                    }
+
+                    if (map == null)
+                    {
+                        map = new HashMap<>();
+                        params.put(pieces[0], map);
+                    }
+
+                    parse(map, pieces[1], value);
+                    return;
+                }
+
+                name = pieces[0];
+            }
+
+            if (params.containsKey(name))
+            {
+                Object existing = params.get(name);
+
+                if (existing instanceof List)
+                {
+                    ((List) existing).add(value);
+                }
+                else
+                {
+                    List<String> l = new ArrayList<>();
+
+                    l.add((String) existing);
+                    l.add(value);
+
+                    params.put(name, l);
+                }
+            }
+            else
+            {
+                params.put(name, value);
+            }
+        }
+
+        public Object get(String name)
+        {
+            return params.get(name);
+        }
+
+        public String single(String name)
+        {
+            Object o = get(name);
+            if (o != null && o instanceof String)
+            {
+                return (String) o;
+            }
+
+            return null;
+        }
     }
 }
